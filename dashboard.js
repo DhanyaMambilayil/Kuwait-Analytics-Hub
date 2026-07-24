d887ec076754cc09cf0d91e90559592742f6ef0c
DhanyaMambilayil <dhanyamambilayil@gmail.com>
2026-07-24 19:22:46 +0300

    dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -122,7 +122,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     row.innerHTML = `
       <div class="dashboard-name-cell">
-        <div class="dashboard-icon">${item.icon || "D"}</div>
+        <div class="dashboard-icon">📊</div>
         <strong>${item.title}</strong>
       </div>
 
@@ -332,4 +332,3 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-
